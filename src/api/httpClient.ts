0c55c60999061c61c6e45909c86d2b1c620e179f
Armando Rivas <rivasarmando271084@icloud.com>
2026-05-28 19:27:36 -0600

    Changed port

--- src/api/httpClient.ts
+++ src/api/httpClient.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "http://localhost:9090/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
